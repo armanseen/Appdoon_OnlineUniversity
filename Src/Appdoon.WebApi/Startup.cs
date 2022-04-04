a97ce79eff06c15b219b4ccf2d67eb16432a5dab
Arman <Armansinaie1380@gmail.com>
2022-04-05 00:06:01 +0430

    some comment added to startup,cs

--- Src/Appdoon.WebApi/Startup.cs
+++ Src/Appdoon.WebApi/Startup.cs
@@ -26,6 +26,7 @@ namespace OU_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //dsfsdfes
 
             //Enable CORS
             services.AddCors(c =>
